0e27b6c7f5ccb65fa4104b93bc1867039192c3ce
Sangamesh <sangameshkaji10@gmail.com>
2025-12-26 17:55:07 +0530

    Q3. Exclusive Time of Functions

--- Data Structures and Algorithms/Linear Shoal/Monotic Stack/Q3.java
+++ Data Structures and Algorithms/Linear Shoal/Monotic Stack/Q3.java
@@ -1,1 +1,27 @@
 //Q3. Exclusive Time of Functions
+import java.util.*;
+class Q3{
+  public int[] exclusiveTime(int n, List<String> logs) {
+    int[] ans = new int[n];
+    Deque<Integer> stack = new ArrayDeque<>();
+    int prevTime = -1;
+
+    for (final String log : logs) {
+      final String[] splits = log.split(":");
+      final int id = Integer.parseInt(splits[0]);        
+      final char label = splits[1].charAt(0);            
+      final int timestamp = Integer.parseInt(splits[2]); 
+      if (label == 's') {
+        if (!stack.isEmpty())
+          ans[stack.peek()] += timestamp - prevTime;
+        stack.push(id);
+        prevTime = timestamp;
+      } else {
+        ans[stack.pop()] += timestamp - prevTime + 1;
+        prevTime = timestamp + 1;
+      }
+    }
+
+    return ans;
+  }
+}
