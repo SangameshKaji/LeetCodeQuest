7ee5328f1a2962119dc70599c167abfa2cc61baf
Sangamesh <sangameshkaji10@gmail.com>
2025-12-27 16:16:57 +0530

    Q2. Evaluate Reverse Polish Notation

---
+++ Data Structures and Algorithms/Linear Shoal/Stack/Q2.java
@@ -0,0 +1,24 @@
+//Q2. Evaluate Reverse Polish Notation
+import java.util.*;
+class Q2 {
+    public int evalRPN(String[] t) {
+        Stack<Integer> s = new Stack<>();
+        for (String t1 : t) {
+            if (!t1.equals("+") && !t1.equals("-") && !t1.equals("*") && !t1.equals("/")) {
+                s.push(Integer.valueOf(t1));
+            } else {
+                int a = s.pop();
+                int b = s.pop();
+                int c = 0;
+                switch (t1) {
+                    case "+" -> c = b + a;
+                    case "-" -> c = b - a;
+                    case "*" -> c = b * a;
+                    case "/" -> c = b / a;
+                }
+                s.push(c);
+            }
+        }
+        return s.pop();
+    }
+}
