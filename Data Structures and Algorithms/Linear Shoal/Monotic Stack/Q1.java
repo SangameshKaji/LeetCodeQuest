78aad42bf7d323cb8bb98d2810d88443bc3e6aa0
Sangamesh <sangameshkaji10@gmail.com>
2025-12-28 22:21:32 +0530

    Q1. Final Prices With a Special Discount in a Shop

--- Data Structures and Algorithms/Linear Shoal/Monotic Stack/Q1.java
+++ Data Structures and Algorithms/Linear Shoal/Monotic Stack/Q1.java
@@ -1,1 +1,24 @@
 //Q1. Final Prices With a Special Discount in a Shop
+import java.util.*;
+class Q1 {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() > prices[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                ans[i] = prices[i];
+            } else {
+                ans[i] = prices[i] - stack.peek();
+            }
+
+            stack.push(prices[i]);
+        }
+
+        return ans;
+    }
+}
