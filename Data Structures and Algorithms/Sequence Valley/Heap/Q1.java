7e3624592ec2388f6d886f428cdb2cf4643f820f
Sangamesh <sangameshkaji10@gmail.com>
2026-01-19 22:59:20 +0530

    Q1. Last Stone Weight

--- Data Structures and Algorithms/Sequence Valley/Heap/Q1.java
+++ Data Structures and Algorithms/Sequence Valley/Heap/Q1.java
@@ -1,4 +1,18 @@
 //Q1. Last Stone Weight
+import java.util.*;
 class Q1 {
-    
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            if (first != second) {
+                pq.offer(first - second);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
+    }
 }
